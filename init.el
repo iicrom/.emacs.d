94d930eb4267ca6340d9eaa7332e6446a7696b12
fufu <yisena@outlook.com>
2026-04-14 00:03:42 +0800

    Add tab-width and default encoding.

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
 ;; -*- lexical-binding: t; -*-
-(setq custom-file "~/.emacs.custom")
+(setq custom-file "~/.emacs.d/.emacs.custom")
 
 (setq inhibit-startup-screen t)
 (menu-bar-mode 0)
@@ -7,7 +7,17 @@
 
 (global-display-line-numbers-mode 1)
 
+(setq-default tab-width 4)
+(setq-default ident-tabs-mode nil)
+
+(delete-selection-mode t)
+
 (set-face-attribute 'default nil :font "JetbrainsMono NFM" :height 160)
+
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
 
 (global-set-key (kbd "C-=") 'text-scale-increase)
 (global-set-key (kbd "C--") 'text-scale-decrease)
